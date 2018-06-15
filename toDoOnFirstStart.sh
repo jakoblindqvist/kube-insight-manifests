35058ac24b0f87da241f468d2f77e33a742d246f
Xartos <freddeliv@hotmail.com>
2018-06-15 15:05:01 +0200

    Fixed problem with isto generated even without argument

--- toDoOnFirstStart.sh
+++ toDoOnFirstStart.sh
@@ -6,12 +6,14 @@ set -e
 # Get and install helm
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
 
-# Get and generate istio deployment
-curl -L https://git.io/getLatestIstio | ISTIO_VERSION=0.8.0 sh -
-cd istio-0.8.0
-helm template install/kubernetes/helm/istio --name istio --namespace metrics --set prometheus.enabled=false > ../istio.yaml
+if [[ $1 == "istio" ]]; then
+  # Get and generate istio deployment
+  curl -L https://git.io/getLatestIstio | ISTIO_VERSION=0.8.0 sh -
+  cd istio-0.8.0
+  helm template install/kubernetes/helm/istio --name istio --namespace metrics --set prometheus.enabled=false > ../istio.yaml
 
-cd ..
+  cd ..
+fi
 
 # Get and generate kube-manifest-insight
 git clone https://github.com/jakoblindqvist/kube-insight-manifests.git
@@ -22,13 +24,15 @@ if [[ $1 == "istio" ]]; then
   extra="--set istioScrape.enabled=true"
 fi
 
-$baseCommand $extra > ./kube-learn-agent.yaml
+$baseCommand $extra > ../kube-learn-agent.yaml
 helm template servers --namespace metrics > ../kube-learn-server.yaml
 
 cd ..
 
 # Deploy to kubernetes
 kubectl create namespace metrics
-kubectl apply -f istio.yaml
+if [[ $1 == "istio" ]]; then
+  kubectl apply -f istio.yaml
+fi
 kubectl apply -f kube-learn-server.yaml
 kubectl apply -f kube-learn-agent.yaml
