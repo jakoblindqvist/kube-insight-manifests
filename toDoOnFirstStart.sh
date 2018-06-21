65807d2f9a1b78e9f49cc984f1709a6be8d02249
jakobl <jakobl@cs.umu.se>
2018-06-21 13:07:21 +0200

    updated setup script to use helm

--- toDoOnFirstStart.sh
+++ toDoOnFirstStart.sh
@@ -1,38 +1,17 @@
 #!/bin/bash
 
-# Exit on any error
-set -e
-
 # Get and install helm
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
 
+# Make sure metrics namespace is initialized
+kubectl create ns metrics
+set -e
+
 if [[ $1 == "istio" ]]; then
-  # Get and generate istio deployment
-  curl -L https://git.io/getLatestIstio | ISTIO_VERSION=0.8.0 sh -
-  cd istio-0.8.0
-  helm template install/kubernetes/helm/istio --name istio --namespace metrics --set prometheus.enabled=false > ../istio.yaml
-
-  cd ..
-fi
-
-# Get and generate kube-manifest-insight
-git clone https://github.com/jakoblindqvist/kube-insight-manifests.git
-cd kube-insight-manifests
-
-baseCommand="helm template agents --namespace metrics"
-if [[ $1 == "istio" ]]; then
+  # curl -L https://git.io/getLatestIstio | ISTIO_VERSION=0.8.0 sh -
   extra="--set istioScrape.enabled=true"
+  helm install istio-0.8.0/install/kubernetes/helm/istio --name istio --namespace metrics --set prometheus.enabled=false
 fi
 
-$baseCommand $extra > ../kube-learn-agent.yaml
-helm template servers --namespace metrics > ../kube-learn-server.yaml
+helm install agents --namespace metrics $extra
 
-cd ..
-
-# Deploy to kubernetes
-kubectl create namespace metrics
-if [[ $1 == "istio" ]]; then
-  kubectl apply -f istio.yaml
-fi
-kubectl apply -f kube-learn-server.yaml
-kubectl apply -f kube-learn-agent.yaml
