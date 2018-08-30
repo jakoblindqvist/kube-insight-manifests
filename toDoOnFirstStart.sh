8b9a48505f8cf6cf85c2f87fe7a4937f2610957d
Fredrik Liv <freddeliv@hotmail.com>
2018-08-30 14:53:03 +0200

    Updated script to use istio 1.0.1

--- toDoOnFirstStart.sh
+++ toDoOnFirstStart.sh
@@ -2,15 +2,16 @@
 
 # Get and install helm
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
+helm init
 
 # Make sure metrics namespace is initialized
 kubectl create ns metrics
 set -e
 
 if [[ $1 == "istio" ]]; then
-  curl -L https://git.io/getLatestIstio | ISTIO_VERSION=0.8.0 sh -
+  curl -L https://git.io/getLatestIstio | ISTIO_VERSION=1.0.1 sh -
   extra="--set istioScrape.enabled=true"
-  helm install istio-0.8.0/install/kubernetes/helm/istio --name istio --namespace metrics --set prometheus.enabled=false
+  helm install istio-1.0.1/install/kubernetes/helm/istio --name istio --namespace metrics --set prometheus.enabled=false
 fi
 
 helm install agents --namespace metrics $extra
